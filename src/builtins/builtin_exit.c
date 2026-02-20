c08aad541ef2a1bff07e75fc96b47a25e9bf4f37
baderelg <bader.elg@gmail.com>
2026-02-20 07:27:06 +0100

    polish: removed wrapper for overflow on exit

--- src/builtins/builtin_exit.c
+++ src/builtins/builtin_exit.c
@@ -6,26 +6,11 @@
 /*   By: baelgadi <baelgadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/11 17:53:47 by baelgadi          #+#    #+#             */
-/*   Updated: 2026/02/20 07:18:49 by baelgadi         ###   ########.fr       */
+/*   Updated: 2026/02/20 07:24:54 by baelgadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-/**
- * @brief Checks if the given argument overflows
- * 
- * @return 1 if overflow, 0 if not
- */
-static int	is_overflow(char *str)
-{
-	long long	val;
-	int			overflow;
-
-	val = ft_atoll(str, &overflow);
-	(void)val;
-	return (overflow);
-}
 
 /**
  * @brief Exit the shell with specific status code
@@ -46,12 +31,13 @@ int	builtin_exit(char **args, t_shell *shell)
 
 	if (shell->interactive)
 		ft_putstr_fd("exit\n", 1);
-	if (ft_arrlen(args) == 1)
+	if (!args[1])
 	{
 		shell->running = 0;
 		return (shell->exit_status);
 	}
-	if (!ft_str_is_numeric(args[1]) || is_overflow(args[1]))
+	exit_code = ft_atoll(args[1], &overflow);
+	if (!ft_str_is_numeric(args[1]) || overflow)
 	{
 		ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
 		ft_putstr_fd(args[1], STDERR_FILENO);
@@ -62,7 +48,6 @@ int	builtin_exit(char **args, t_shell *shell)
 	if (ft_arrlen(args) > 2)
 		return (ft_putstr_fd("minishell: exit: too many arguments\n",
 				STDERR_FILENO), 1);
-	exit_code = ft_atoll(args[1], &overflow);
 	shell->running = 0;
 	return ((unsigned char)exit_code);
 }
